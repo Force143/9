119d058a7f230be1d006970ceaebc2ccf38a268b
Force143 <77406489+Force143@users.noreply.github.com>
2025-03-22 17:41:28 +0000

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -79,9 +79,9 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.value === 20) inner.textContent = "1 1";
   if (tile.value === 22) inner.textContent = "շ";
   if (tile.value === 25) inner.textContent = "Ƽ";
-  if (tile.value === 26) inner.textContent = "б";
+  if (tile.value === 26) inner.textContent = "ϭ";
   if (tile.value === 27) inner.textContent = "Ꞁ";
-  if (tile.value === 38) inner.textContent = "𝟾";
+  if (tile.value === 38) inner.textContent = "𝟪";
   if (tile.value === 41) inner.textContent = "𝟭";
   if (tile.value === 44) inner.textContent = "Ɥ";
   if (tile.value === 91) inner.textContent = "𝟵";
